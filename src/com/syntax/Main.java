c1087bff90e5853e62a8a4f05d403e542f95bed0
VioletTemenuzhka <violet.chan.fl@gmail.com>
2022-05-18 00:07:39 -0400

    The settings of the car are set to 88MHZ

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -10,5 +10,10 @@ public class Main {
         Audi.engineCapac="V8";
         Audi.headlights="blue";
         Audi.regNo="tuzik";
+
+        //lets write our own code
+        System.out.println("Welcome to my car");
+        System.out.println("The radio is set to 88MHZ");
+        System.out.println("Turn on the AC");
     }
 }
